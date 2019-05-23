8185615667438590f317f8aedac27aec88344709
Dmitriy Sidenko <dmsidenko@gmail.com>
2019-05-24 02:47:42 +0300

    uuid fix

--- custom/modules/Opportunities/hooks/create_new_task_hook.php
+++ custom/modules/Opportunities/hooks/create_new_task_hook.php
@@ -76,21 +76,6 @@ class create_new_task_hook
 
         //**********************************************************
         $now = date_create()->format('Y-m-d H:i:s');
-        $uuid = sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
-            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
-            // 16 bits for "time_mid"
-            mt_rand( 0, 0xffff ),
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
-            mt_rand( 0, 0x0fff ) | 0x4000,
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand( 0, 0x3fff ) | 0x8000,
-            // 48 bits for "node"
-            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
-        );
 
         //***************Get contact_id****************************
         $dbGetContactId = DBManagerFactory::getInstance();
@@ -308,6 +293,21 @@ class create_new_task_hook
 //        $dbInsertNewTask->query($queryInsertNewTask);
         if ($is_need_task_to_engineer){
             $GLOBALS['log']->warn('is_need_task_to_engineer');
+            $uuid = sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                // 32 bits for "time_low"
+                mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+                // 16 bits for "time_mid"
+                mt_rand( 0, 0xffff ),
+                // 16 bits for "time_hi_and_version",
+                // four most significant bits holds version number 4
+                mt_rand( 0, 0x0fff ) | 0x4000,
+                // 16 bits, 8 bits for "clk_seq_hi_res",
+                // 8 bits for "clk_seq_low",
+                // two most significant bits holds zero and one for variant DCE1.1
+                mt_rand( 0, 0x3fff ) | 0x8000,
+                // 48 bits for "node"
+                mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+            );
             $dbInsertNewTask = DBManagerFactory::getInstance();
             $task = new Task();
             $task->id = $uuid;
@@ -333,6 +333,21 @@ class create_new_task_hook
             foreach ($coordinator_ids as $coordinator_id) {
                 $dbInsertNewTask = DBManagerFactory::getInstance();
                 $task = new Task();
+                $uuid = sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                    // 32 bits for "time_low"
+                    mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+                    // 16 bits for "time_mid"
+                    mt_rand( 0, 0xffff ),
+                    // 16 bits for "time_hi_and_version",
+                    // four most significant bits holds version number 4
+                    mt_rand( 0, 0x0fff ) | 0x4000,
+                    // 16 bits, 8 bits for "clk_seq_hi_res",
+                    // 8 bits for "clk_seq_low",
+                    // two most significant bits holds zero and one for variant DCE1.1
+                    mt_rand( 0, 0x3fff ) | 0x8000,
+                    // 48 bits for "node"
+                    mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+                );
                 $task->id = $uuid;
                 $task->name = $name;
                 $task->created_by = $bean->created_by;
